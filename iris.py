1fa9a04d26c222df55f125eaa722da2bdef9523a
taylor-ortiz <ortiz.taylor@gmail.com>
2023-08-09 06:23:36 -0600

    adding wine quality py and dataset to repo

--- iris.py
+++ iris.py
@@ -18,15 +18,21 @@ y = df.iloc[:, -1].values
 # Split the dataset into an 80-20 training-test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-y_train = y_train.reshape(-1, 1)
-y_test = y_test.reshape(-1, 1)
 
 # Apply feature scaling on the training and test sets
 sc = StandardScaler()
+
 X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
+
+X_test = sc.transform(X_test)  # Corrected line
+
+y_train = y_train.reshape(-1, 1)
+
+y_test = y_test.reshape(-1, 1)
+
 y_train = sc.fit_transform(y_train)
-y_test = sc.fit_transform(y_test)
+
+y_test = sc.transform(y_test)  # Corrected line if scaling is necessary.
 
 # Print the scaled training and test sets
 print(X_train)
